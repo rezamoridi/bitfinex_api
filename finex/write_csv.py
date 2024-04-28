c6a930824256b8c2849ef89e49a3eb23984d8e2d
rezamoridi <rezamoridi2042@gmail.com>
2024-04-28 03:53:47 +0330

    int fixed

--- finex/write_csv.py
+++ finex/write_csv.py
@@ -1,12 +1,12 @@
 import csv
-import datetime
+from datetime import datetime
 
 def save_to_csv(data, filename):
     with open(filename, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(["Timestamp", "Open", "High", "Low", "Close", "Volume"])
         for candle in data:
-            timestamp = datetime.fromtimestamp(candle[0] / 1000).strftime('%Y-%m-%d %H:%M:%S')
+            timestamp = datetime.fromtimestamp(int(candle[0]) / 1000).strftime('%Y-%m-%d %H:%M:%S')
             open_price = candle[1]
             high_price = candle[3]
             low_price = candle[4]
