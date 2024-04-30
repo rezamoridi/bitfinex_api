b245c76b4a992372161741920d1b36d5e7f68a11
rezamoridi <rezamoridi2042@gmail.com>
2024-04-30 17:40:55 +0330

    ticker fixed

--- finex/bitfinex_api.py
+++ finex/bitfinex_api.py
@@ -41,9 +41,7 @@ def get_bitfinex_price_data(symbol, start_date, end_date, timeframe):
     return None
 
 
-import requests
-import time
-from datetime import datetime
+
 
 def get_bitfinex_api_ticker(symbol, sleep):
     url = f"https://api-pub.bitfinex.com/v2/ticker/{symbol}"
@@ -75,14 +73,14 @@ def convert_timesnap(timesnap):
 
 headers = ["BID", "BID_SIZE", "ASK", "ASK_SIZE", "DAILY_CHANGE", "DAILY_CHANGE_PERCENT", "LAST_PRICE", "VOLUME", "HIGH", "LOW", "TIMESTAMP", "TIMESTAMP_CONVERTED"]
 
-with open(file=f"second_csv.csv", mode="a") as f:
+with open(file=f"second.csv", mode="a") as f:
     f.write(','.join(headers) + '\n') 
 
 while True:
     data = get_bitfinex_api_ticker("tBTCUSD", sleep=0)
     data.append(convert_timesnap(data[-1]))  
     print(data)
-    with open(file="mm.csv", mode="a") as f:  
+    with open(file="second.csv", mode="a") as f:  
         f.write(','.join(map(str, data)) + '\n')  
     time.sleep(1)
   
