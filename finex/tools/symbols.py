ed22456137e66687f2a575a0202309335c7f212d
rezamoridi <rezamoridi2042@gmail.com>
2024-05-02 03:22:27 +0330

    convert to functional module to fetch/update symbols in csv file

--- finex/tools/symbols.py
+++ finex/tools/symbols.py
@@ -1,5 +1,24 @@
 import requests
+import csv
 
-response = requests.get(url="https://api-pub.bitfinex.com/v2/conf/pub:list:pair:exchange")
+def fetch_symbols(symbols_list : list = None, heads : list = None) -> bool:
 
-symbols = sorted(list(response.json()[0]))
+    if symbols_list and heads:
+        symbols = symbols_list
+        headers = heads
+    else:
+        response = requests.get(url="https://api-pub.bitfinex.com/v2/conf/pub:list:pair:exchange")
+        headers = ['Symbols']
+        symbols = response.json()[0]
+
+    with open("../csv/symbols_csv/symbolcsv.csv", mode="+w") as symbolcsv:
+        writer = csv.writer(symbolcsv)
+
+        writer.writerow(headers)
+        for symbol in symbols:
+            writer.writerow([symbol])
+    
+    return True
+
+if __name__ == "__main__":
+    fetch_symbols()
