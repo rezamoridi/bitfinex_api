9fa63d29fc56bb1dbd0dc1010ff9f8d2a3608b0f
rezamoridi <rezamoridi2042@gmail.com>
2024-05-05 14:40:36 +0330

    bitstamp wss

--- websocket-finex.py
+++ websocket-finex.py
@@ -1,7 +1,6 @@
 import asyncio
 import websockets
 import json
-import time
 
 async def heartbeat(websocket, interval):
     while True:
@@ -9,28 +8,34 @@ async def heartbeat(websocket, interval):
         await asyncio.sleep(interval)  # Wait for the next heartbeat interval
 
 async def subscribe_to_ticker():
-    uri = "wss://api-pub.bitfinex.com/ws/2"
+    uri = "wss://ws.bitstamp.net"
 
-    async with websockets.connect(uri) as websocket:
-        # Subscribe to the ticker channel for BTC/USD pair
-        await websocket.send('{"event": "subscribe", "channel": "ticker", "symbol": "tBTCUSD"}')
+    while True:
+        try:
+            async with websockets.connect(uri) as websocket:
+                # Subscribe to the ticker channel for BTC/USD pair
+                await websocket.send(json.dumps({"event": "bts:subscribe", "data": {"channel": "live_trades_btcusd"}}))
 
-        # Start the heartbeat task with a shorter interval (e.g., 5 seconds)
-        asyncio.create_task(heartbeat(websocket, 1))
+                # Start the heartbeat task with a shorter interval (e.g., 5 seconds)
+                asyncio.create_task(heartbeat(websocket, 1))
 
-        datas = {}
-        # Receive and process messages
-        async for message in websocket:
-            # Parse the JSON message
-            data = json.loads(message)
+                # Receive and process messages
+                async for message in websocket:
+                    # Parse the JSON message
+                    data = json.loads(message)
 
-            
-            # Check if it's a ticker update message
-            if isinstance(data, list) and len(data) > 1 and isinstance(data[1], list) and len(data[1]) > 6:
-                # Extract the last price from the ticker update
-                last_price = data[1][0]
-                last_avg = {last_price: last_price/2}
-                print(f"Last price of BTC/USD: {last_avg}")
+                    # Check if it's a live trades message
+                    if 'data' in data and 'amount' in data['data'] and 'price' in data['data']:
+                        amount = data['data']['amount']
+                        price = data['data']['price']
+                        print(f"Last trade: {amount} BTC at ${price} USD")
+        except websockets.exceptions.ConnectionClosedError:
+            print("Connection to the WebSocket closed unexpectedly. Retrying...")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        finally:
+            # Add a delay before reconnecting
+            await asyncio.sleep(5)
 
 # Run the WebSocket client
 asyncio.get_event_loop().run_until_complete(subscribe_to_ticker())
